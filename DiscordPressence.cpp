235a346a542067288e2c3a52c2d54b6ef4290d1c
Urafael <105573199+UrafaelGames@users.noreply.github.com>
2025-01-11 01:59:11 -0500

    Update DiscordPressence.cpp

--- DiscordPressence.cpp
+++ DiscordPressence.cpp
@@ -1,62 +1,74 @@
 #include "DiscordPressence.h"
 
-DiscordPressence::DiscordPressence() : core(nullptr){}
+DiscordPressence::DiscordPressence() : core( nullptr )
+{ }
 
-DiscordPressence::~DiscordPressence() {
-  shutdown();
+DiscordPressence::~DiscordPressence()
+{
+    // shutdown();
 }
 
-DiscordPressence* DiscordPressence::getInstance(){
-  static DiscordPressence instance;
-  return &instance;
+DiscordPressence* DiscordPressence::getInstance()
+{
+    static DiscordPressence instance;
+    return &instance;
 }
 
-bool DiscordPressence::init(){
+bool DiscordPressence::init()
+{
+    if (core) {
+        CCLOG( "Discord already initialized." );
+        return true;
+    }
+    // Here you can add your discord client ID, first create an application in discord developer portal, after OAuth2 and you can show your client id, copy and pste on 'YOUR_CLIENT_ID'
+    discord::Result result = discord::Core::Create( YOUR_CLIENT_ID, DiscordCreateFlags_Default, &core );
 
-	uint64_t flags = static_cast<uint64_t>(discord::CreateFlags::NoRequireDiscord);
-
-     // Here you can add your Discord client ID, where discord developer portal / Applications
-	discord::Result result = discord::Core::Create( YOUR_CLIENT_ID, flags, &core);
-
-	if (result == discord::Result::Ok){
-		CCLOG( "Discord initialized succesfully" );
-		return true;
-	} else {
-	   CCLOG("Failed to initialized Discord: %d", result);
-	   return false;
-	}
+    if (result == discord::Result::Ok) {
+        CCLOG( "Discord initialized successfully" );
+        return true;
+    }
+    else {
+        CCLOG( "Failed to initialize Discord: %d", static_cast<int>(result) );
+        return false;
+    }
 }
 
-void DiscordPressence::updatePressence(const std::string& state, const std::string& details) {
-	 if (!core) return;
+void DiscordPressence::updatePresence( const std::string& state, const std::string& details )
+{
+    if (!core) {
+        CCLOG( "Discord core is not initialized." );
+        return;
+    }
 
-   auto& activityManager = core->ActivityManager();
-   discord::Activity activity{};
-   activity.SetState(state.c_str());
-   activity.SetDetails(details.c_str());
-   activity.GetAssets().SetLargeImage("your_logo"); // Here you can add your game logo, from Discord developer portal / Applications / Rich Pressence / Art assets
-   activity.GetAssets().SetLargeText("your_game_name"); // Add your game name
+    auto& activityManager = core->ActivityManager();
+    discord::Activity activity{ };
+    activity.SetState( state.c_str() );
+    activity.SetDetails( details.c_str() );
+    activity.GetAssets().SetLargeImage( "your_game_logo" ); // in discord dev portal add your game icon, in the rout application->Rich Pressence->art assets
+    activity.GetAssets().SetLargeText( "your_game_name" ); // your name application
 
-   activityManager.UpdateActivity(activity, [](discord::Result result) {
-   	 if (result == discord::Result::Ok)	{
-	   CCLOG("Discord presence updated succesfully");
-	 } else {
-	   CCLOG("Failed to update Discord pressence: %d", static_cast<int>(result));
-	 }
-   });
-
+    activityManager.UpdateActivity( activity, []( discord::Result result ) {
+        if (result == discord::Result::Ok) {
+            CCLOG( "Discord presence updated successfully" );
+        }
+        else {
+            CCLOG( "Failed to update Discord presence: %d", static_cast<int>(result) );
+        }
+    } );
 }
 
-void DiscordPressence::shutdown() {
-  if (core) {
-   delete core;
-   core = nullptr;
-   CCLOG("Discord core shutdown succesfully");
-  }
+void DiscordPressence::shutdown()
+{
+    if (core) {
+        core = nullptr;
+        CCLOG( "Discord core shutdown successfully" );
+    }
 }
 
-void DiscordPressence::tick() {
-  if(core) {
-   core->RunCallbacks();
-  }
+void DiscordPressence::tick()
+{
+    if (core) {
+        core->RunCallbacks();
+    }
 }
+                 
