6c70ba670024e14c95ecc49323a8cfe5fd459f09
Urafael <105573199+UrafaelGames@users.noreply.github.com>
2025-01-11 01:58:40 -0500

    Update DiscordPressence.h

--- DiscordPressence.h
+++ DiscordPressence.h
@@ -2,21 +2,25 @@
 #define __DISCORDPRESSENCE_H__
 
 #include "discord_game_sdk.h"
-#include "discord/discord.h"
-#include "cocos2d.h"
+#include "Discord/discord.h"
+#include "Discord/core.h"
+#include "CCConsole.h"
+#include <string>
 
-class DiscordPressence {
+class DiscordPressence
+{
 private:
-	DiscordPressence();
-	~DiscordPressence();
+    DiscordPressence();
+    ~DiscordPressence();
+
+    discord::Core* core;
+
 public:
-	discord::Core* core; 
-	static DiscordPressence* getInstance();
-   bool init();
-   void updatePressence(const std::string& state = "Playing", const std::string& details = "");
-   void shutdown();
-   void tick();
+    static DiscordPressence* getInstance();
+    bool init();
+    void updatePresence( const std::string& state = "Playing", const std::string& details = "" );
+    void shutdown();
+    void tick();
 };
 
-
-#endif // !__DISCORDPRESSENCE_H__
+#endif // __DISCORDPRESSENCE_H__
